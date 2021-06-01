112f32d5cf17037c084a8dd677d1200f97bb5c93
huang yanlin <2510555317@qq.com>
2021-06-01 14:38:35 +0800

    footerguide

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <FooterGuide/>
+  </div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+export default {
+  components: {
+    FooterGuide
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+.app {
+  color: read;
+}
+</style>
