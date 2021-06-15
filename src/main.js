2db594f6a14f904718bd7e1b57052e6a1887b6b9
huang yanlin <2510555317@qq.com>
2021-06-15 10:30:05 +0800

    finish

--- src/main.js
+++ src/main.js
@@ -2,12 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import {Button} from 'mint-ui'
+import VueLazyload from 'vue-lazyload'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './mock/mockServer' // 加载mockServer即可
+import loading from './common/imgs/loading.gif'
+import './filters' // 加载过滤器
 import 'default-passive-events'
 Vue.config.productionTip = false
+Vue.use(VueLazyload, { // 内部自定义一个指令lazy
+  loading
+})
 // 注册全局组件标签
 Vue.component(Button.name, Button) // <mt-button>
 /* eslint-disable no-new */
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import MSite from '../pages/MSite/MSite.vue'
-import Search from '../pages/Search/Search.vue'
-import Order from '../pages/Order/Order.vue'
-import Profile from '../pages/Profile/Profile.vue'
 import Login from '../pages/Login/Login.vue'
 import Shop from '../pages/Shop/Shop.vue'
 import ShopGoods from '../pages/Shop/ShopGoods/ShopGoods.vue'
 import ShopRatings from '../pages/Shop/ShopRatings/ShopRatings.vue'
 import ShopInfo from '../pages/Shop/ShopInfo/ShopInfo.vue'
 Vue.use(VueRouter)
-
+const MSite = () => import('../pages/MSite/MSite.vue')
+const Search = () => import('../pages/Search/Search.vue')
+const Order = () => import('../pages/Order/Order.vue')
+const Profile = () => import('../pages/Profile/Profile.vue')
 export default new VueRouter({
   routes: [
     {
@@ -22,7 +21,7 @@ export default new VueRouter({
     },
     {
       path: '/msite',
-      component: MSite,
+      component: MSite, // 返回路由组件的函数，只有执行函数的时候才会加载路由组件，这个函数在请求对呀的路由路径时才会执行
       meta: {
         showFooter: true
       }
